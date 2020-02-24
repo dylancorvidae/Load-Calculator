0601fb63680ff6ba810189f60f3c0a02a9a92a2d
Dylan Corvidae <50845869+dylancorvidae@users.noreply.github.com>
2020-02-24 13:38:33 -0800

    updates

--- src/loadCalculator.js
+++ src/loadCalculator.js
@@ -1,25 +1,11 @@
-// export function characteristicLoad() {
-//   const performerMass = document.getElementById('performer-mass').value;
-//   const apparatusMass = document.getElementById('apparatus-mass').value;
-//   const totalMass = performerMass + apparatusMass;
-  
-//   const dynamicFactor = document.getElementById('dynamic-factor').value;
-
-//   let result = document.getElementById('result');
-
-//   let characteristicLoad = totalMass * dynamicFactor * 6;
-//   result.value = characteristicLoad;
-// }
-
-
 // Stretch Goal
 // add staticLoad characteristicLoad = totalMass * 10 whichever is greater
 
-export function characteristicLoad() {
+function characteristicLoad() {
   const performerMass = document.getElementById('performer-mass').value;
   const apparatusMass = document.getElementById('apparatus-mass').value;
   const totalMass = performerMass + apparatusMass;
-  
+
   const dynamicFactor = document.getElementById('dynamic-factor').value;
 
   let result = document.getElementById('result');
@@ -28,3 +14,4 @@ export function characteristicLoad() {
   result.value = characteristicLoad;
 }
 
+document.getElementById('calculate').addEventListener('click', characteristicLoad);

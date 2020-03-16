de2c9612accbdee325e461a6edfcd99654431e43
Dylan Corvidae <50845869+dylancorvidae@users.noreply.github.com>
2020-03-15 21:37:02 -0700

    working, but needs refactoring and to add color switch on button and select

--- src/themeSwitch.js
+++ src/themeSwitch.js
@@ -4,19 +4,19 @@ const currentTheme = localStorage.getItem('theme');
 if(currentTheme) {
   document.documentElement.setAttribute('data-theme', currentTheme);
   
-  if(currentTheme === 'dark') {
+  if(currentTheme === 'light') {
     toggleSwitch.checked = true;
   }
 }
 
 function switchTheme(e) {
   if(e.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
-  }
-  else {        
     document.documentElement.setAttribute('data-theme', 'light');
     localStorage.setItem('theme', 'light');
+  }
+  else {        
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
   }    
 }
 
